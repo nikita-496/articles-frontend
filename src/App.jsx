f65a41f06ae7bc59ea3233e5da8c28e4fed331d1
Nikita Shevkunov <shevkunovnikita97@gmail.com>
2022-10-05 15:06:14 +0500

    rendering components through a container

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Navbar from './components/bars/navbar/Navbar'
-import Home from './pages/home/Home'
 import Profile from './pages/profile/Profile';
 import Article from './pages/article/Article';
+import HomeContainer from './pages/home/HomeContainer';
 
 function App() {
   const buttons = ['Зарегистрироваться', 'Войти']
@@ -12,7 +12,7 @@ function App() {
     <Router>
     <Navbar navItems={buttons}/>
     <Routes>
-      <Route path='/' element={<Home />} />
+      <Route path='/' element={<HomeContainer />} />
       <Route path='/profile' element={<Profile />} />
       <Route path='/article' element={<Article />} />
     </Routes>
