9d7d13e851f41dc1ea600899df48f52d24f506d4
Nikita Shevkunov <shevkunovnikita97@gmail.com>
2022-10-05 19:49:27 +0500

    creating a field configuration utility

---
+++ src/utils/fieldsForm/loginForm.js
@@ -0,0 +1,18 @@
+export default {
+  login: {
+    label: 'Логин',
+    elementConfig: {
+      type: 'text',
+      name: 'login',
+    },
+    value: '',
+  },
+  password: {
+    label: 'Пароль',
+    elementConfig: {
+      type: 'password',
+      name: 'password',
+    },
+    value: '',
+  },
+};
