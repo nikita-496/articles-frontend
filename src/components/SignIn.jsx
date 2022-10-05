ad7b2715af139bfc750558c17fd148d7273bad30
Nikita Shevkunov <shevkunovnikita97@gmail.com>
2022-10-05 19:57:34 +0500

    creating a user sign_in component

---
+++ src/components/SignIn.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import loginForm from '../utils/fieldsForm/loginForm';
+import personService from '../services/personService';
+import localStorageManager from '../persistentStorge/localStorageManager';
+
+import CustomButton from './customMUI/customButton/CustomButton';
+import CustomInput from './customMUI/customInput';
+import withForm from '../hocs/withForm';
+
+const SignIn = (props) => {
+  const data = {
+    login: props.formState.login.value,
+    password: props.formState.password.value,
+  };
+
+  const navigate = useNavigate();
+
+  const handleLogin = (evt) => {
+    props.postJSON(evt, personService.login, data).then((res) => {
+      localStorageManager.set('user', res);
+      navigate(`/profile/${res.userInfo.login}`);
+    });
+  };
+
+  return (
+    <form
+      sx={{
+        width: 500,
+        maxWidth: '100%',
+      }}
+      style={{ 'margin-top': '100px', width: '500px', maxWidth: '100%' }}
+      onSubmit={(evt) => handleLogin(evt)}
+    >
+      {props.onRenderElements().map((el) => (
+        <CustomInput
+          key={el.id}
+          label={el.config.label}
+          name={el.config.elementConfig.name}
+          type={el.config.elementConfig.type}
+          value={el.config.value}
+          changed={(evt) => props.onChangeForm(evt, el.id)}
+        />
+      ))}
+      <CustomButton inner={"Войти"}/>
+    </form>
+  );
+};
+
+export default withForm(loginForm)(SignIn);
