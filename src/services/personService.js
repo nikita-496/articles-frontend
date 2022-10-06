833a1ce5bccdcd01d30191ac2aba9c6ceb1fbd21
Nikita Shevkunov <shevkunovnikita97@gmail.com>
2022-10-06 05:21:44 +0500

    extension of person services by registration of person method

--- src/services/personService.js
+++ src/services/personService.js
@@ -1,8 +1,27 @@
 import { API, postJSON } from './api';
 import profileService from './profileService';
+import handleDefaultEntity from '../utils/handleDefaultEntity';
+
+const register = async (payload) => {
+  let registeredPerson = await postJSON(API.person, payload);
+  registeredPerson = registeredPerson.data;
+
+  const newProfile = await handleDefaultEntity.createNew(API.profile);
+  await handleDefaultEntity.update(API.profile, {
+    id: newProfile.id,
+    user_id: registeredPerson.id,
+  });
+
+  const newFeed = await handleDefaultEntity.createNew(API.feed);
+  await handleDefaultEntity.update(API.feed, {
+    id: newFeed.id,
+    profile_id: newProfile.id,
+  });
+
+  return registeredPerson;
+};
 
 const login = async (payload) => {
-
   const authenticated = await postJSON(API.login, payload);
   const { id } = authenticated.data;
 
@@ -14,8 +33,8 @@ const login = async (payload) => {
   );
 
   const userFeed = await postJSON(API.joingProfile, { profile_id: profileOfAuthenticated[0].id });
-  
+
   return { userInfo: userInfo.data[0], userFeed: userFeed.data[0] };
 };
 
-export default { login };
+export default { register, login };
