d5792e8759c5bdd8e653de800ecd73255c490858
Nikita Shevkunov <shevkunovnikita97@gmail.com>
2022-10-16 06:12:39 +0500

    removing a component of user info

--- src/components/content/cardItem/cardItem.jsx
+++ src/components/content/cardItem/cardItem.jsx
@@ -4,21 +4,12 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import UserInfo from '../../userInfo/userInfo';
 
 const CardItem = (props) => {
   return (
     <Card sx={{ maxWidth: 345 }}>
       <CardMedia component='img' height='140' image={props.poster} alt={props.alt} />
       <CardContent>
-        <UserInfo avatar={
-          {
-            src: props.avatar,
-            alt: props.authorName,
-          }
-        }>
-          {props.children}
-        </UserInfo>
         {props.date}
         <Typography gutterBottom variant='h4'>
           {props.title}
