1e433ecfa959ada58367d1ed4e4084085d4b14f1
Nikita Shevkunov <shevkunovnikita97@gmail.com>
2022-10-16 07:10:46 +0500

    route link to container

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Navbar from './components/bars/navbar/Navbar'
-import Profile from './pages/profile/Profile';
 import Article from './pages/article/Article';
 import HomeContainer from './pages/home/HomeContainer';
 import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
+import ProfileContainer from './pages/profile/ProfileContainer';
+import ArticleContaier from './pages/article/ArticleContainer';
 
 function App() {
   const buttons = ['Зарегистрироваться', 'Войти']
@@ -17,8 +18,8 @@ function App() {
       <Route path='/' element={<HomeContainer />} />
       <Route path='/sign_in' element={<SignIn />} />
       <Route path='/sign_up' element={<SignUp />} />
-      <Route path='/profile' element={<Profile />} />
-      <Route path='/article' element={<Article />} />
+      <Route path='/profile/:login' element={<ProfileContainer />} />
+      <Route path='/article/:id' element={<ArticleContaier />} />
     </Routes>
   </Router>
   );
